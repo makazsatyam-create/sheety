0ce287b811bf86dfc8b9eb85519b423bb5231149
makazsatyam-create <mazaksatyam@gmail.com>
2026-02-26 17:03:59 +0530

    fix:-added the new launch according to client.

--- shetty/client/src/components/home/NewLaunch.jsx
+++ shetty/client/src/components/home/NewLaunch.jsx
@@ -3,8 +3,33 @@ import HomeGameSection from "./HomeGameSection";
 import SpribeData from "../../data/spribe.json";
 
 function NewLaunch() {
-  const games = Array.isArray(SpribeData) ? SpribeData : [];
-  return <HomeGameSection title="New Launch" games={games} />;
+  // Get games from JSON
+  const jsonGames = Array.isArray(SpribeData) ? SpribeData : [];
+
+  // Add additional games
+  const additionalGames = [
+    {
+      id: 37,
+      game_name: "Dragon Tiger",
+      game_uid: "efdb52994fbfe97efcbd878dbd697ebb",
+      game_type: "CasinoLive",
+      provider: "ezugi",
+      icon: "https://bulkapi.in/EZUGI/EZUGI_150.png",
+    },
+    {
+      id: 29,
+      game_name: "Chicken Road 2.0",
+      game_uid: "562b299961b0ec40f252a832453c67b0",
+      game_type: "Instant",
+      provider: "inout",
+      icon: "https://bulkapi.in/Inout/INOUT_chicken-road-2-0.png",
+    },
+  ];
+
+  // Combine both arrays
+  const allGames = [...additionalGames, ...jsonGames];
+
+  return <HomeGameSection title="New Launch" games={allGames} />;
 }
 
 export default NewLaunch;
