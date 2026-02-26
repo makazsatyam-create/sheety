5d419cab93f0f15777a7bb1d32630a92dd2e6d22
makazsatyam-create <mazaksatyam@gmail.com>
2026-02-26 17:06:24 +0530

    fix:-added recomended games towerX.

--- shetty/client/src/components/home/RecomendedGames.jsx
+++ shetty/client/src/components/home/RecomendedGames.jsx
@@ -4,7 +4,17 @@ import InoutData from "../../data/inout.json";
 
 function RecomendedGames() {
   const games = Array.isArray(InoutData) ? InoutData : [];
-  return <HomeGameSection title="Recomended Games" games={games} />;
+  const additionalGames = [
+    {
+      id: 13,
+      game_name: "TowerX",
+      game_uid: "5fa811450c40fda4caba581f6f10f2aa",
+      game_type: "XGames",
+      icon: "https://bulkapi.in/smart_soft/683fee631b4d58c4b7c00370_TowerX.webp",
+    },
+  ];
+  const allGames = [...additionalGames, ...games];
+  return <HomeGameSection title="Recomended Games" games={allGames} />;
 }
 
 export default RecomendedGames;
